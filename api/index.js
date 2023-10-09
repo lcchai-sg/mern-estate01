0ee80da2f6bdb544277fc761492746bbdd1a37db
lcchai <lc.chai.sg@gmail.com>
2023-10-09 15:30:37 +0800

    connect mongo db

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import "dotenv/config";
+import mongoose from "mongoose";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log(`connected to mongo DB`);
+    })
+    .catch((err) => {
+        console.log(error);
+    });
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
