3262fcf09a9982d8ec8c0c483e68e4d876a179a4
lcchai <lc.chai.sg@gmail.com>
2023-10-09 15:17:34 +0800

    api

---
+++ api/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import "dotenv/config";
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
