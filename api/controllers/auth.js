8bbc19e5bb1073263e38f75bb6288b2741daa7bb
lcchai <lc.chai.sg@gmail.com>
2023-10-16 15:04:11 +0800

    signin api

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,6 @@
 import User from "../models/user.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
@@ -20,5 +21,30 @@ export const signup = async (req, res, next) => {
         res.status(201).json(user);
     } catch (error) {
         next(errorHandler(590, "error in signup function!", error));
+    }
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return next(errorHandler(404, "Invalid credentials!"));
+        }
+        const validPassword = bcryptjs.compareSync(password, user.password);
+        if (!validPassword) {
+            return next(errorHandler(401, "Invalid credentials!"));
+        }
+        const { password: pass, ...userInfo } = user._doc;
+        const token = jwt.sign(
+            { id: user._id, isAdmin: user.isAdmin },
+            process.env.JWT_KEY,
+            {}
+        );
+        res.cookie("access_token", token, { httpOnly: true })
+            .status(200)
+            .json(userInfo);
+    } catch (error) {
+        next(errorHandler(590, "error in signin function!", error));
     }
 };
--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ mongoose
         console.log(`connected to mongo DB`);
     })
     .catch((err) => {
-        console.log(error);
+        console.log(err);
     });
 
 app.listen(PORT, () => {
@@ -26,7 +26,7 @@ app.use("/api/auth", authRouter);
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal Server Error";
-    const details = err.details || "no details available";
+    const details = err.details || "details not available";
     return res.status(statusCode).json({
         success: false,
         statusCode,
